1823c01706b888069fd55df856189b954f58e484
wemetsite-cloud <wemetsite@gmail.com>
2026-08-24 20:42:33 +0530

    Delete we-met-platform-github-safe/employee-site directory

--- we-met-platform-github-safe/employee-site/config.js
+++
@@ -1,7 +0,0 @@
-window.PORTAL_CONFIG = Object.freeze({
-  API_BASE_URL: '',
-  TOKEN_KEY: 'we_met_listener_token',
-  EXPECTED_ROLE: 'employee',
-  BRAND_NAME: 'We Met',
-  SUPPORT_EMAIL: 'wemetsite@gmail.com'
-});
